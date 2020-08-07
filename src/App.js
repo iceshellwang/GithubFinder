e20810465f290481f8c8536d77df732d23e15c10
Alex <35946230+iceshellwang@users.noreply.github.com>
2020-08-07 23:48:23 +1000

    setState for users and loading

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
   async componentDidMount() {
+    this.setState({ loading: true });
     const res = await axios.get('https://api.github.com/users');
-    console.log(res.data);
+    //console.log(res.data);
+    this.setState({ users: res.data, loading: false });
   }
   render() {
     return (
