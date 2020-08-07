c90a216f8661813f01cee47cb00be319202115c1
Alex <35946230+iceshellwang@users.noreply.github.com>
2020-08-07 23:40:30 +1000

    fetch data from GitHubAPI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/layout/Navbar'
+import axios from 'axios';
+import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='container'>
-        <Users /></div>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
