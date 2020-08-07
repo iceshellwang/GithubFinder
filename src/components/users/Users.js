b84554c27e7a2e63d03a9394b827d4ecd1b2c38a
Alex <35946230+iceshellwang@users.noreply.github.com>
2020-08-07 23:54:38 +1000

    fetch all users' data from github API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,33 +3,12 @@ import UserItem from './UserItem'
 
 
 class Users extends Component {
-    state = {
-        users: [
-            {
-                "login": "mojombo",
-                "id": 1,
-                "node_id": "MDQ6VXNlcjE=",
-                "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
-            },
-            {
-                "login": "defunkt",
-                "id": 2,
-                "node_id": "MDQ6VXNlcjI=",
-                "avatar_url": "https://avatars0.githubusercontent.com/u/2?v=4",
-            },
-            {
-                "login": "pjhyett",
-                "id": 3,
-                "node_id": "MDQ6VXNlcjM=",
-                "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
-            }
-        ]
-    }
+    
     render() {
         return (
             <div style={userStyle}>
                 {
-                    this.state.users.map(user => (
+                    this.props.users.map(user => (
                         <UserItem key={user.id} user={user} />
                     ))
                 }
