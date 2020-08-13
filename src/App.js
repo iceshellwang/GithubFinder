64ded0cd71a037d9b8d8bf76509568a0662e821e
Alex <35946230+iceshellwang@users.noreply.github.com>
2020-08-13 23:38:14 +1000

    transfer props from APP to single user component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
-import About from './components/pages/About'
+import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Search from './components/users/Search';
@@ -13,7 +13,7 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
   };
   // async componentDidMount() {
 
@@ -22,30 +22,28 @@ class App extends Component {
   //   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
   //   this.setState({ users: res.data, loading: false });
   // }
-  searchUsers = async text => {
-    this.setState({ loading: true })
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false });
-  }
+  };
   //search single user
   getUser = async (username) => {
-    this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/search/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ user: res.data, loading: false });
-
-  }
+  };
   clearUsers = () => {
-    this.setState({ users: [], loading: false })
-  }
+    this.setState({ users: [], loading: false });
+  };
   setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } })
-    setTimeout(() => this.setState({ alert: null }), 5000)
-
-  }
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
   render() {
-    const { loading, users, } = this.state
+    const { loading, users } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -53,21 +51,37 @@ class App extends Component {
           <div className='container'>
             <Alert alert={this.state.alert} />
             <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}
-                    setAlert={this.setAlert} />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )} />
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
               <Route exact path='/about' component={About} />
-              <Route exact path='/user:login' render={props => (
-                <Fragment>
-                  <User {...props} getUser={this.getUser} user={this.state.user} />
-                </Fragment>
-              )} />
+              <Route
+                exact
+                path='/user:login'
+                render={(props) => (
+                  <Fragment>
+                    <User
+                      {...props}
+                      getUser={this.getUser}
+                      user={this.state.user}
+                      loading={this.state.loading}
+                    />
+                  </Fragment>
+                )}
+              />
             </Switch>
-
           </div>
         </div>
       </Router>
