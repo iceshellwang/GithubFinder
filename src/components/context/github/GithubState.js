7698cea3b702caaf7d2fd2229901a95108605bf6
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-20 23:38:54 +1000

    structure GithubState

--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -4,11 +4,30 @@ import githubReducer from './githubReducer'
 import axios from 'axios'
 import { SEARCH_USERS, SET_LOADING, GET_REPOS, GET_USER, CLEAR_USERS } from '../types'
 
-const GithubState = () => {
+const GithubState = props => {
     const initialState = {
         users: [],
         user: {},
         loading: false,
         repos: []
     }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    //actions
+
+
+    return <GithubContext.Provider
+        value={{
+            users: state.users,
+            user: state.user,
+            repos: state.repos,
+            loading: state.loading
+
+        }}
+    >
+        {props.children}
+
+    </GithubContext.Provider>
 }
+export default GithubState
