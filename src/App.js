885f7395d213dcb839ef9b79f512754d3c57602d
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-20 23:54:10 +1000

    dispatch setLoading in GithubState

--- src/App.js
+++ src/App.js
@@ -23,14 +23,7 @@ const App = () => {
   //   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
   //   this.setState({ users: res.data, loading: false });
   // }
-  const searchUsers = async (text) => {
-    setLoading(true)
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUsers(res.data.items)
-    setLoading(false)
 
-  };
   //search single user
   const getUser = async (username) => {
     setLoading(true)
--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -15,7 +15,18 @@ const GithubState = props => {
     const [state, dispatch] = useReducer(GithubReducer, initialState)
 
     //actions
+    const searchUsers = async (text) => {
+        setLoading(true)
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        setUsers(res.data.items)
+        setLoading(false)
 
+    };
+
+    const setLoading = () => {
+        dispatch({ type: SET_LOADING })
+    }
 
     return <GithubContext.Provider
         value={{
