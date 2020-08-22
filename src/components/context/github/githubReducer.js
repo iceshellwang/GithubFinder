018ef392408561d92d920cd92bbb191c5f7a2ab1
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-22 23:47:30 +1000

    add searchUsers in reducer

--- src/components/context/github/githubReducer.js
+++ src/components/context/github/githubReducer.js
@@ -2,6 +2,12 @@ import { SEARCH_USERS, SET_LOADING, GET_REPOS, GET_USER, CLEAR_USERS } from '../
 
 export default (state, action) => {
     switch (action, type) {
+        case SEARCH_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                loading: false
+            }
         case SET_LOADING:
             return {
                 ...state,
