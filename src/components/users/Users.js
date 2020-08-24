195c926568ac9b9768efd3520fe54b3909af8327
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-24 23:53:50 +1000

    rm non-existing required propTypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -17,10 +17,7 @@ const Users = () => {
     );
   }
 };
-// Users.propTypes = {
-//   users: PropTypes.array.isRequired,
-//   loading: PropTypes.bool.isRequired
-// };
+
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3,1fr)',
