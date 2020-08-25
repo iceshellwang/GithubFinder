be4516c6ab1801f940956ad4789105cc7b71a604
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-25 23:14:41 +1000

    add getUserRepos in GithubState

--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -33,14 +33,15 @@ const GithubState = props => {
 
     };
     const clearUsers = () => dispatch({ type: CLEAR_USERS })
-    // const getUserRepos = async (username) => {
-    //     setLoading(true)
-    //     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    //   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    //     setRepos(res.data)
-    //     dispatch({ type: GET_REPOS, payload: res.data })
 
-    // };
+    //get users repo
+    const getUserRepos = async (username) => {
+        setLoading()
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+  &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        dispatch({ type: GET_REPOS, payload: res.data })
+    };
+
     //clear users
 
     const setLoading = () => {
@@ -55,7 +56,8 @@ const GithubState = props => {
             loading: state.loading,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
 
         }}
     >
