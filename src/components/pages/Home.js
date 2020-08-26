390effd833fb89dc502b4a39a0f13291524f5092
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-08-26 23:48:17 +1000

    introduce Home component to clean App.js

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Search from '../users/Search';
+import Users from './components/users/Users';
+const Home = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default Home
